45f4bba9d23c53f0be6545ef059a3a66b00e1352
AhmedKhalil777 <progeng_ahmed_khalil@outlook.com>
2019-06-25 03:37:06 +0200

    Registeration Controlling For Users

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using market.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -15,11 +16,44 @@ namespace market.Controllers
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signManager;
 
-        public AccountController(UserManager<IdentityUser> userManager ,SignInManager<IdentityUser> signManager)
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager)
         {
             _userManager = userManager;
             _signManager = signManager;
         }
 
+        [HttpPost("action")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel formdata)
+        {
+            // Will Hold all The errors related to Registration
+            List<string> errorList = new List<string> {};
+
+            var user = new IdentityUser
+            {
+                Email = formdata.Email,
+                UserName = formdata.Username,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var result = await _userManager.CreateAsync(user, formdata.Password);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+
+                return Ok(new {username = user.UserName ,email = user.Email, status = 1,message ="Registeration Succesful"});
+                
+            }
+
+            else
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                    errorList.Add(error.Description);
+                }
+            }
+            return BadRequest(new JsonResult(errorList));
+        }
     }
 }
