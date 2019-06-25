5f1ed655c181844c689d3a576efacf941511e794
AhmedKhalil777 <progeng_ahmed_khalil@outlook.com>
2019-06-26 00:41:28 +0200

    Login Contolling

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using market.Helpers;
 using market.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace market.Controllers
 {
@@ -13,13 +19,18 @@ namespace market.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        //dependency Injection
+
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signManager;
+        private readonly AppSettings _appSettings;
 
-        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager)
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager ,IOptions<AppSettings> appSettings)
         {
             _userManager = userManager;
             _signManager = signManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost("action")]
@@ -54,6 +65,60 @@ namespace market.Controllers
                 }
             }
             return BadRequest(new JsonResult(errorList));
+        }
+
+        // Login Method 
+        [HttpPost("action")]
+        public async Task<IActionResult> login([FromBody]LoginViewModel formdata)
+        {
+            // find a user
+            var user = await _userManager.FindByNameAsync(formdata.Username);
+
+            // find a Role
+            var roles = await _userManager.GetRolesAsync(user);
+
+            //defining Key
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appSettings.Secret));
+
+            // Expire time defining
+            double tokenExpiryTime = double.Parse( _appSettings.ExpireTime);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user,formdata.Password))
+            {
+                // Confirmation Email
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenDiscriptor = new SecurityTokenDescriptor {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, formdata.Username),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(ClaimTypes.NameIdentifier, user.Id),
+                        new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
+                        new Claim("LoggedOn", DateTime.Now.ToString()),
+
+
+                    }),
+                    SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature),
+                    Issuer = _appSettings.Site,
+                    Audience = _appSettings.Audience,
+                    Expires = DateTime.UtcNow.AddMinutes(tokenExpiryTime)
+                    
+                };
+
+
+                // generate Token
+                var token = tokenHandler.CreateToken(tokenDiscriptor);
+                return Ok(new { token =tokenHandler.WriteToken(token), expiration = token.ValidTo, username = user.UserName ,userRole = roles.FirstOrDefault()
+            });
+
+
+
+            }
+
+            // login error 
+            ModelState.AddModelError("", "Username/password are not found !");
+            return Unauthorized(new { LoginError = "Please check the login credentials - invalid username / password" });
         }
     }
 }
