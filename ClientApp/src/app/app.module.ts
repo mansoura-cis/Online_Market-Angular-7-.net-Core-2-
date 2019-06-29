512343dc723daa467616f47917e3412f55a630d0
AhmedKhalil777 <progeng_ahmed_khalil@outlook.com>
2019-06-29 23:41:57 +0200

    Front end Starting

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
+    ProductsComponent,
+    HomeComponent,
     LoginComponent,
-    RegisterComponent,
-    ProductsComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
